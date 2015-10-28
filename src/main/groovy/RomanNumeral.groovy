6e791b1ffcc830ee9649f77e6198fd77c2a70d4f
German Gonzalez <German_Gonzalez@gap.com>
2015-10-28 10:39:56 -0300

    Casos hasta el 6, usando if. luego viene case

--- src/main/groovy/RomanNumeral.groovy
+++ src/main/groovy/RomanNumeral.groovy
@@ -1,5 +1,3 @@
-
-
 public class RomanNumeral {
 
     private int value
@@ -9,6 +7,14 @@ public class RomanNumeral {
     }
 
     String toString() {
-        return "I"*value
+        if (value < 4)
+            return "I" * value
+        else if (value == 4)
+            return "IV"
+        else if (value == 5)
+            return "V"
+        else if (value == 6)
+            return "VI"
+        else return "VII"
     }
 }
--- src/test/groovy/RomanNumeralTest.groovy
+++ src/test/groovy/RomanNumeralTest.groovy
@@ -1,13 +1,11 @@
 import org.junit.Test
-
-import static junit.framework.Assert.assertEquals
+import static org.junit.Assert.assertEquals;
 
 class RomanNumeralTest {
 
     @Test
     public void canTransformOneToRomanNumber() {
         assertEquals("I", new RomanNumeral(1).toString())
-
     }
 
     @Test(expected = GroovyRuntimeException.class)
@@ -23,5 +21,25 @@ class RomanNumeralTest {
     @Test
     public void when3ThenReturnIII() {
         assertEquals("III", new RomanNumeral(3).toString())
+    }
+
+    @Test
+    public void when4ThenReturnIV() throws Exception {
+        assertEquals("IV",new RomanNumeral(4).toString())
+    }
+
+    @Test
+    public void when5ThenReturnV() throws Exception {
+        assertEquals("V", new RomanNumeral(5).toString())
+    }
+
+    @Test
+    public void when6ThenReturnVI() throws Exception {
+        assertEquals("VI", new RomanNumeral(6).toString())
+    }
+
+    @Test
+    public void when7ThenReturnVII() throws Exception {
+        assertEquals("VII", new RomanNumeral(7).toString())
     }
 }
